ec71910d99f32123c4b45f30935d88093d7b3843
WilhenAlbertoHM <wahm0131@gmail.com>
2024-12-18 15:32:47 -0500

    edit: move social icons back to navbar

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -15,17 +15,9 @@ const About = () => {
                     </h1>
                     <p className="aboutText">
                         Hola! I’m Wilhen Alberto Hui Mei, and I have 1 year 
-                        of experience in building AI and ML applications. 
-                        I’m focused in using spoken and programming languages to create a positive impact around us.
+                        of experience in building software and AI/ML applications. 
+                        I use spoken and programming languages to create a positive impact around us.
                     </p>
-                </div>
-                <div className="aboutSocial">
-                    <a href="https://github.com/WilhenAlbertoHM/" className="aboutSocialIcon">
-                        <i className="fab fa-github" id="github-icon"></i>
-                    </a>
-                    <a href="https://www.linkedin.com/in/wilhenalbertohm/" className="aboutSocialIcon">
-                        <i className="fab fa-linkedin" id="linkedin-icon"></i>
-                    </a>
                 </div>
             </div>
         </div>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -13,6 +13,15 @@ const NavBar = () => {
                 <Link className="navLink" to="about" smooth={true} duration={600}>About</Link>
                 <Link className="navLink" to="experienceList" smooth={true} duration={600}>Experience</Link>
                 <Link className="navLink" to="projectList" smooth={true} duration={600}>Projects</Link>
+                <Link className="navLink" to="gallery" smooth={true} duration={600}>Gallery</Link>
+            </div>
+            <div className="aboutSocial">
+                <a href="https://github.com/WilhenAlbertoHM/" className="aboutSocialIcon">
+                    <i className="fab fa-github" id="github-icon"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/wilhenalbertohm/" className="aboutSocialIcon">
+                    <i className="fab fa-linkedin" id="linkedin-icon"></i>
+                </a>
             </div>
         </header>
     );
