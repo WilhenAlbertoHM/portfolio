b83c7074341cbdcd023791978cd4fba48aaa8da7
WilhenAlbertoHM <wahm0131@gmail.com>
2024-12-24 13:49:27 -0500

    add: test deploy

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,24 @@
 import React, { useState } from "react";
-import "./App.css";
-import NavBar from "../NavBar/NavBar.jsx";
-import Intro from "../Intro/Intro.jsx";
-import About from "../About/About.jsx";
-import ExperienceList from "../Experience/ExperienceList.jsx";
-import ProjectList from "../Projects/ProjectList.jsx";
-import PictureList from "../Gallery/PictureList.jsx";
-import TechStackList from "../TechStack/TechStackList.jsx";
+// import "./App.css";
+// import NavBar from "../NavBar/NavBar.jsx";
+// import Intro from "../Intro/Intro.jsx";
+// import About from "../About/About.jsx";
+// import ExperienceList from "../Experience/ExperienceList.jsx";
+// import ProjectList from "../Projects/ProjectList.jsx";
+// import PictureList from "../Gallery/PictureList.jsx";
+// import TechStackList from "../TechStack/TechStackList.jsx";
 
 const App = () => {
     return (
         <div>
-            <NavBar />
+            {/* <NavBar />
             <Intro />
             <About />
             <ExperienceList />  
             <ProjectList />
             <TechStackList />
-            <PictureList />
+            <PictureList /> */}
+            Hello World
         </div>
     );
 }
