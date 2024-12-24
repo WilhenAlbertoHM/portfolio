d0bfea67ca41b17d003527aed9d0d1a0e3e860e5
WilhenAlbertoHM <wahm0131@gmail.com>
2024-12-24 15:41:18 -0500

    add: fix deploy

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import NavBar from "./components/NavBar/NavBar.jsx";
 import Intro from "./components/Intro/Intro.jsx";
 import About from "./components/About/About.jsx";
-import ExperienceList from "../Experience/ExperienceList.jsx";
+import ExperienceList from ".components/Experience/ExperienceList.jsx";
 import ProjectList from "./components/Projects/ProjectList.jsx";
 import PictureList from "./components/Gallery/PictureList.jsx";
 import TechStackList from "./components/TechStack/TechStackList.jsx";
