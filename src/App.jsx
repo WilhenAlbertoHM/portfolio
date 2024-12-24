a8d623572eb563b65187f6e329d5822c078c14a3
WilhenAlbertoHM <wahm0131@gmail.com>
2024-12-24 15:39:57 -0500

    add: fix deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import "./App.css";
-import NavBar from "../NavBar/NavBar.jsx";
-import Intro from "../Intro/Intro.jsx";
-import About from "../About/About.jsx";
+import NavBar from "./components/NavBar/NavBar.jsx";
+import Intro from "./components/Intro/Intro.jsx";
+import About from "./components/About/About.jsx";
 import ExperienceList from "../Experience/ExperienceList.jsx";
-import ProjectList from "../Projects/ProjectList.jsx";
-import PictureList from "../Gallery/PictureList.jsx";
-import TechStackList from "../TechStack/TechStackList.jsx";
+import ProjectList from "./components/Projects/ProjectList.jsx";
+import PictureList from "./components/Gallery/PictureList.jsx";
+import TechStackList from "./components/TechStack/TechStackList.jsx";
 
 const App = () => {
     return (
