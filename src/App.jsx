a4f7140cef7018d1f586a5583c57f4a65f85bfbe
WilhenAlbertoHM <wahm0131@gmail.com>
2024-12-24 15:56:30 -0500

    add: fix deploy

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar.jsx";
+import Intro from "./components/Intro/Intro.jsx";
+import About from "./components/About/About.jsx";
+import ExperienceList from "./components/Experience/ExperienceList.jsx";
+import ProjectList from "./components/Projects/ProjectList.jsx";
+import PictureList from "./components/Gallery/PictureList.jsx";
+import TechStackList from "./components/TechStack/TechStackList.jsx";
+
+const App = () => {
+    return (
+        <div>
+            <NavBar />
+            <Intro />
+            <About />
+            <ExperienceList />  
+            <ProjectList />
+            <TechStackList />
+            <PictureList />
+        </div>
+    );
+}
+
+export default App
