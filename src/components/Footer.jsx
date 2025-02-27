f80aa7c3935bddffcc9639e8a8939724d8072fca
WilhenAlbertoHM <wahm0131@gmail.com>
2025-02-26 21:59:05 -0500

    add: edit footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 export const Footer = () => {
     return (
         <footer>
-            <div className="flex justify-center items-center p-4 bg-[var(--black)] text-[var(--lightgray)]">
-                <p>&copy; 2025 Wilhen Alberto Hui Mei. Almost all rights reserved</p>
+            <div className="flex justify-center items-center p-4 bg-[var(--black)]">
+                <p className="text-[var(--lightgray)] italic">&copy; Copyright Wilhen Alberto Hui Mei, 2025. Almost all rights reserved</p>
             </div>
         </footer>
     );  
